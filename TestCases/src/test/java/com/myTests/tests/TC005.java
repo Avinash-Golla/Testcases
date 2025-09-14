c4a123a7cd759034287a750a620a189a4b451bfc
Avinash Golla <137623743+Avinash-Golla@users.noreply.github.com>
2025-09-14 23:47:53 +0530

    Add files via upload

---
+++ TestCases/src/test/java/com/myTests/tests/TC005.java
@@ -0,0 +1,11 @@
+package com.myTests.tests;
+
+import java.util.*;
+class TC005
+{
+   public static void main(String args[])
+    {
+        System.out.println("This testcase result is tc005");
+    }
+
+}
---
+++ TestCases/src/test/java/com/myTests/tests/TC008.java
@@ -0,0 +1,11 @@
+package com.myTests.tests;
+
+import java.util.*;
+class TC008
+{
+   public static void main(String args[])
+    {
+        System.out.println("This testcase result is tc008");
+    }
+
+}
