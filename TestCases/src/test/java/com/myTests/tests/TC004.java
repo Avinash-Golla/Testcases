96cc2da769faf9f41f3c7e10c065b755aec285fa
Avinash Golla <137623743+Avinash-Golla@users.noreply.github.com>
2025-09-15 02:40:45 +0530

    Update TC004.java

--- TestCases/src/test/java/com/myTests/tests/TC004.java
+++ TestCases/src/test/java/com/myTests/tests/TC004.java
@@ -4,11 +4,12 @@ import java.util.*;
 class TC004
 {
    @Test
-   public void testcase003()
+   public void testcase004()
     {
         System.out.println("This testcase result is tc004");
     }
 
 
 }
+
 
