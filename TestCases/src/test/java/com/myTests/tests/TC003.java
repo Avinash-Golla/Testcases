988c8b7754e46880fc63c780da27af01b6aabe60
Avinash Golla <137623743+Avinash-Golla@users.noreply.github.com>
2025-09-15 02:22:21 +0530

    Update TC003.java

--- TestCases/src/test/java/com/myTests/tests/TC003.java
+++ TestCases/src/test/java/com/myTests/tests/TC003.java
@@ -3,9 +3,11 @@ package com.myTests.tests;
 import java.util.*;
 class TC003
 {
+   @Test
    public static void main(String args[])
     {
         System.out.println("This testcase result is tc003");
     }
+
 
 }
