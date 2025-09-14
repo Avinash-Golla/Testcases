3ae474d993947a94a80ac16239591b37c266a7d4
Avinash Golla <137623743+Avinash-Golla@users.noreply.github.com>
2025-09-15 02:23:51 +0530

    Update TC008.java

--- TestCases/src/test/java/com/myTests/tests/TC008.java
+++ TestCases/src/test/java/com/myTests/tests/TC008.java
@@ -3,9 +3,11 @@ package com.myTests.tests;
 import java.util.*;
 class TC008
 {
+   @Test
    public static void main(String args[])
     {
         System.out.println("This testcase result is tc008");
     }
+
 
 }
